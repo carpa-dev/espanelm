283bfda3e082c2af71ed3634cba4e2e3dbb7408a
Daniela Yassuda <daniela.yassuda@quintoandar.com.br>
2019-07-22 22:41:08 -0300

    Cleanup unused stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import 'normalize.css';
-import './main.css';
-import {Elm} from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 import 'bulma/css/bulma.css';
-
-import 'tailwindcss/dist/tailwind.min.css';
+import { Elm } from './Main.elm';
+import registerServiceWorker from './registerServiceWorker';
+import './main.css';
 
 Elm.Main.init({
   node: document.getElementById('root'),
